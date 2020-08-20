af7397f90a773afb99c44adf7443a6148f62867b
Kanwar gill <gill.nihal@live.com>
2020-08-19 20:49:20 -0700

    2.5,2.6

--- exercises/exercise-2.js
+++ exercises/exercise-2.js
@@ -86,4 +86,34 @@ const deleteGreeting = async (req, res) => {
   client.close();
 };
 
-module.exports = { createGreeting, getGreeting, getGreetings, deleteGreeting };
+const updateGreeting = async (req, res) => {
+  const _id = req.params._id;
+  const client = await MongoClient(MONGO_URI, options);
+  try {
+    await client.connect();
+    const db = client.db("exercise-2");
+    const newValues = { $set: { hello: req.body.hello } };
+    console.log(req.body.hello);
+    if (req.body.hello === undefined) {
+      throw new Error("Enter something");
+    }
+    const r = await db.collection("greetings").updateOne({ _id }, newValues);
+    assert.equal(1, r.matchedCount);
+    assert.equal(1, r.modifiedCount);
+    res.status(200).json({ status: 200, data: { ...req.body } });
+  } catch (err) {
+    console.log(err.stack);
+    res
+      .status(500)
+      .json({ status: 500, data: { ...req.body }, message: err.message });
+  }
+  client.close();
+};
+
+module.exports = {
+  createGreeting,
+  getGreeting,
+  getGreetings,
+  deleteGreeting,
+  updateGreeting,
+};
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const {
   getGreeting,
   getGreetings,
   deleteGreeting,
+  updateGreeting,
 } = require("./exercises/exercise-2");
 
 const PORT = process.env.PORT || 8000;
@@ -33,6 +34,7 @@ express()
   .get("/exercise-2/greeting/:_id", getGreeting)
   .get("/exercise-2/greeting", getGreetings)
   .delete("/exercise-2/greeting/:_id", deleteGreeting)
+  .put("/exercise-2/greeting/:_id", updateGreeting)
   // handle 404s
   .use((req, res) => res.status(404).type("txt").send("🤷‍♂️"))
 
